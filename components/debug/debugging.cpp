4655c5c17f83952be729b5d834f415468e121558
dwing <dwing4g@gmail.com>
2024-04-12 11:55:08 +0800

    openmw-cn: v1-v10

--- components/debug/debugging.cpp
+++ components/debug/debugging.cpp
@@ -357,7 +357,9 @@ int wrapApplication(int (*innerApplication)(int argc, char* argv[]), int argc, c
     rawStderrMutex = std::make_unique<std::mutex>();
 
     int ret = 0;
+#if defined(NDEBUG)
     try
+#endif
     {
         if (const auto env = std::getenv("OPENMW_DISABLE_CRASH_CATCHER");
             env == nullptr || Misc::StringUtils::toNumeric<int>(env, 0) == 0)
@@ -383,6 +385,7 @@ int wrapApplication(int (*innerApplication)(int argc, char* argv[]), int argc, c
         else
             ret = innerApplication(argc, argv);
     }
+#if defined(NDEBUG)
     catch (const std::exception& e)
     {
 #if (defined(__APPLE__) || defined(__linux) || defined(__unix) || defined(__posix))
@@ -394,6 +397,7 @@ int wrapApplication(int (*innerApplication)(int argc, char* argv[]), int argc, c
 
         ret = 1;
     }
+#endif
 
     // Restore cout and cerr
     std::cout.rdbuf(rawStdout->rdbuf());
